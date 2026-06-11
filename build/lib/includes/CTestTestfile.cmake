03e9fc7b07134122dcdcecbf861f85a3e9dca8c9
Justin Woo <justinwoo280@gmail.com>
2026-06-11 13:53:42 +0000

    Remove build/ artifacts from git tracking
    The CMake build directory (147 files including .a, .so, CMakeCache)
    was accidentally committed. Add build/ to .gitignore.

--- build/lib/includes/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/project/nghttp3/lib/includes
-# Build directory: /root/project/nghttp3/build/lib/includes
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
